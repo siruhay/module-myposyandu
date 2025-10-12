774b7081de9c6a95dc66377e4fa35b023fc9866b
sudarmono <30992530+monoland@users.noreply.github.com>
2025-10-12 08:35:11 +0700

    update

--- frontend/pages/complaint/crud/edit.vue
+++ frontend/pages/complaint/crud/edit.vue
@@ -1,13 +1,72 @@
 <template>
 	<form-edit with-helpdesk>
-		<template v-slot:default="{ record }">
+		<template v-slot:default="{ combos: { services }, record }">
 			<v-card-text>
 				<v-row dense>
 					<v-col cols="12">
 						<v-text-field
-							label="Name"
+							autocomplete="off"
+							label="Nama Warga"
 							v-model="record.name"
+							hide-details
 						></v-text-field>
+					</v-col>
+
+					<v-col cols="6">
+						<v-select
+							:items="services"
+							autocomplete="off"
+							label="Bidang"
+							v-model="record.service_id"
+							hide-details
+						></v-select>
+					</v-col>
+
+					<v-col cols="6">
+						<v-date-input
+							autocomplete="off"
+							prepend-icon=""
+							label="Tanggal"
+							v-model="record.date"
+							hide-details
+						></v-date-input>
+					</v-col>
+
+					<v-col cols="12">
+						<v-textarea
+							autocomplete="off"
+							label="Keterangan"
+							v-model="record.description"
+							hide-details
+						></v-textarea>
+					</v-col>
+				</v-row>
+			</v-card-text>
+
+			<div class="text-overline px-4">dokumen</div>
+			<v-divider></v-divider>
+
+			<v-card-text>
+				<v-row dense>
+					<v-col
+						v-for="(document, documentIndex) in record.paths"
+						:key="documentIndex"
+						cols="12"
+					>
+						<file-upload
+							:accept="document.mime"
+							:label="document.name"
+							:extension="document.extension"
+							:slug="document.slug"
+							:callback="(res) => (document.path = res.path)"
+							v-model="document.path"
+							backend-url="/myposyandu/api/upload-document"
+							density="comfortable"
+							deletable
+							hide-details
+							readonly
+							uploadable
+						></file-upload>
 					</v-col>
 				</v-row>
 			</v-card-text>
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Module\MyPosyandu\Http\Controllers\DashboardController;
+use Module\MyPosyandu\Http\Controllers\MyPosyanduReportController;
 use Module\MyPosyandu\Http\Controllers\MyPosyanduActivityController;
 use Module\MyPosyandu\Http\Controllers\MyPosyanduComplaintController;
 
@@ -13,3 +14,5 @@ Route::delete('upload-document', [DashboardController::class, 'destroy']);
 Route::resource('complaint', MyPosyanduComplaintController::class)->parameters(['activity' => 'myPosyanduComplaint']);
 
 Route::resource('activity', MyPosyanduActivityController::class)->parameters(['activity' => 'myPosyanduActivity']);
+
+Route::resource('report', MyPosyanduReportController::class)->parameters(['report' => 'myPosyanduReport']);
